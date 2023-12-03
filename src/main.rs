d8c167a6de0ba6115497424e74d32b7c0022cc08
Andrew Hepp <andrew.hepp@ahepp.dev>
2023-12-03 00:03:31 -0500

    hello, dioxus

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use axum::{routing::get, Router};
+use axum::{response::Html, routing::get, Router};
 use dioxus::prelude::*;
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/", get(app_endpoint));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn app_endpoint() -> Html<String> {
+    Html(dioxus_ssr::render_lazy(rsx! {
+        div { "Hello, World!" }
+    }))
 }
